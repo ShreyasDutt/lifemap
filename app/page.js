cfe3bc90241f7e5fdb2a51178972340c9966003c
Shreyas Dutt <151562739+ShreyasDutt@users.noreply.github.com>
2025-06-20 00:11:08 +0530

    todos 4 tomorrow

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ const page = () => {
   return (
     <div className='flex items-center justify-center mt-20'>
       <h1>
-        Create Models and DbConnection <br/>
+        Create Models and DbConnection & Create Webhooks for CRUD operations on User <br/>
         Install Rough.js <br/>
         Create Poloroid Card Component <br/>
         Connect Cloudinary
