e029cc41c56b6c20c0a8ceda81542fe506139508
Shreyas Dutt <151562739+ShreyasDutt@users.noreply.github.com>
2025-06-20 23:17:47 +0530

    Todos for tomorrow

--- app/page.js
+++ app/page.js
@@ -4,11 +4,14 @@ const page = () => {
   return (
     <div className='flex items-center justify-center mt-20'>
       <h1>
-        <p className='line-through'>Create Models and DbConnection <br/></p>
+        <p className='line-through'>Create Models and DbConnection <br/>
         Create Webhooks for CRUD operations on User <br/>
-        Install Rough.js <br/>
+        </p>
+        Create a form to add a memory <br/>
         Create Poloroid Card Component <br/>
-        Connect Cloudinary
+        Connect Cloudinary <br/>
+        Find a Scribble Library for Ui <br/>
+
 
       </h1>
     </div>
